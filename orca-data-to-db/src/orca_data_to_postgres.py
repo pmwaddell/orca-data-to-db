93720b2ae89ee6a4f5415f5ba531ffde811f1624
pmwaddell <pmwaddell9@gmail.com>
2024-03-05 20:55:50 -0500

    Add ability to put ORCA data from PostgreSQL into Mage

--- orca-data-to-db/src/orca_data_to_postgres.py
+++ orca-data-to-db/src/orca_data_to_postgres.py
@@ -1,10 +1,12 @@
-import os
 import pandas as pd
+import psycopg2
 from sqlalchemy import create_engine
 
 
-json_name = '../data/ORCA_data_PPh3_test_input.json'
-df = pd.read_json(json_name)  # not sure about index_col=False...
+csv_name = '../data/ORCA_data_PPh3_test_input.csv'
+
+# TODO: run on all files with .csv, concatenate w pandas I suppose
+df = pd.read_csv(csv_name)
 print(df.head())
 
 engine = create_engine('postgresql://root:root@localhost:5431/orca_data')
